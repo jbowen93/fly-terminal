a830f83723bf160f92edd8f0a343441e3329a2ee
Fly Dev <dev@fly.io>
2025-06-23 23:47:17 +0000

    create Chat context

---
+++ lib/chat_app/chat.ex
@@ -0,0 +1,43 @@
+defmodule ChatApp.Chat do
+  @moduledoc """
+  The Chat context for managing messages.
+  """
+
+  import Ecto.Query, warn: false
+  alias ChatApp.Repo
+  alias ChatApp.Chat.Message
+
+  @doc """
+  Returns the list of messages ordered by most recent first.
+  """
+  def list_messages do
+    Message
+    |> order_by(desc: :inserted_at)
+    |> Repo.all()
+  end
+
+  @doc """
+  Creates a message and broadcasts it to all connected clients.
+  """
+  def create_message(attrs \\ %{}) do
+    %Message{}
+    |> Message.changeset(attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, message} ->
+        # Broadcast the new message to all connected clients
+        Phoenix.PubSub.broadcast(ChatApp.PubSub, "chat:lobby", {:new_message, message})
+        {:ok, message}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking message changes.
+  """
+  def change_message(%Message{} = message, attrs \\ %{}) do
+    Message.changeset(message, attrs)
+  end
+end
