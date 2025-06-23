d4b75c593fa2c3d497a0c12a7f2c3484c3f5b431
Fly Dev <dev@fly.io>
2025-06-23 23:47:35 +0000

    create ChatLive LiveView

---
+++ lib/chat_app_web/live/chat_live.ex
@@ -0,0 +1,75 @@
+defmodule ChatAppWeb.ChatLive do
+  use ChatAppWeb, :live_view
+
+  alias ChatApp.Chat
+  alias ChatApp.Chat.Message
+
+  @impl true
+  def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(ChatApp.PubSub, "chat:lobby")
+    end
+
+    messages = Chat.list_messages()
+    changeset = Chat.change_message(%Message{})
+
+    {:ok,
+     socket
+     |> assign(:form, to_form(changeset))
+     |> assign(:username, "guest_#{:rand.uniform(9999)}")
+     |> assign(:messages_empty?, messages == [])
+     |> stream(:messages, messages)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"message" => message_params}, socket) do
+    changeset =
+      %Message{}
+      |> Chat.change_message(message_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, form: to_form(changeset))}
+  end
+
+  @impl true
+  def handle_event("send_message", %{"message" => message_params}, socket) do
+    message_params = Map.put(message_params, "username", socket.assigns.username)
+
+    case Chat.create_message(message_params) do
+      {:ok, message} ->
+        changeset = Chat.change_message(%Message{})
+
+        {:noreply,
+         socket
+         |> assign(:form, to_form(changeset))
+         |> assign(:messages_empty?, false)
+         |> stream_insert(:messages, message, at: -1)}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
+  end
+
+  @impl true
+  def handle_event("set_username", %{"username" => username}, socket) do
+    {:noreply, assign(socket, :username, username)}
+  end
+
+  @impl true
+  def handle_info({:new_message, message}, socket) do
+    {:noreply,
+     socket
+     |> assign(:messages_empty?, false)
+     |> stream_insert(:messages, message, at: -1)}
+  end
+
+  defp format_time(datetime) do
+    datetime
+    |> DateTime.from_naive!("Etc/UTC")
+    |> Calendar.strftime("%H:%M:%S")
+  end
+
+  defp scroll_to_bottom(js \\ %JS{}) do
+    JS.dispatch(js, "phx:scroll-to-bottom", to: "#messages-container")
+  end
+end
