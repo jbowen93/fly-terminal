c51d9f30c28d567b67ad5a78422f1b1bde00fa2c
Fly Dev <dev@fly.io>
2025-06-24 00:26:44 +0000

    add health action to PageController

--- lib/chat_app_web/controllers/page_controller.ex
+++ lib/chat_app_web/controllers/page_controller.ex
@@ -2,6 +2,14 @@ defmodule ChatAppWeb.PageController do
   use ChatAppWeb, :controller
 
   def home(conn, _params) do
+    def health(conn, _params) do
+      json(conn, %{status: "ok"})
+    end
+
     render(conn, :home)
+
+    def health(conn, _params) do
+      json(conn, %{status: "ok"})
+    end
   end
 end
